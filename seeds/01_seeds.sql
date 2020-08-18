0ea0fd748eb1a1f36f1d0dc21b68248490fbc04c
Victoria Lee <victorialee72@live.com>
2020-08-18 19:26:46 +0000

    Fixed bugs

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -3,10 +3,10 @@ VALUES ('Victoria', 'victoria@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.J
 ('Patricia', 'patricia@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 ('Zoey', 'zoey@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, parking_space, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-VALUES (1, 'Pool House', 'description', 'url', 'url', 5, 4, 8, 'Canada', 'Bayview', 'Richmondhill', 'Ontario', 'L8F9F3', true),
-(2, 'Spa House', 'description', 'url', 'url', 4, 3, 6, 'Canada', 'Yonge', 'North York', 'Ontario', 'L6M2M6', true),
-(3, 'Gym House', 'description', 'url', 'url', 2, 2, 4, 'Canada', 'Kennedy', 'Markham', 'Ontario', 'L4F6O3', true);
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (1, 'Pool House', 'description', 'url', 'url', 300, 5, 4, 8, 'Canada', 'Bayview', 'Richmondhill', 'Ontario', 'L8F9F3', true),
+(2, 'Spa House', 'description', 'url', 'url', 200, 4, 3, 6, 'Canada', 'Yonge', 'North York', 'Ontario', 'L6M2M6', true),
+(3, 'Gym House', 'description', 'url', 'url', 250, 2, 2, 4, 'Canada', 'Kennedy', 'Markham', 'Ontario', 'L4F6O3', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2018-09-01', '2018-09-09', 1, 2),
